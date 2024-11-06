b9ef2621feaa998a56fdad75c78c79093f9944fc
Nn17 <yakor356@gmail.com>
2024-11-06 18:42:42 +0300

    init commit

---
+++ src/main/java/edu/rut/mongophd/MongoPhdApplication.java
@@ -0,0 +1,13 @@
+package edu.rut.mongophd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoPhdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoPhdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/rut/mongophd/MongoPhdApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.rut.mongophd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoPhdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
